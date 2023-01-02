70d3f07d652cfbff803217f6ae90d2bda50bd0fc
Jean Wylmer <jean_wylmer@hotmail.com>
2023-01-02 13:54:17 -0300

    init: estrutura basica do projeto

---
+++ back-end/src/server.ts
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+    'Authorization',
+  ],
+  methods: 'GET,PUT,POST,DELETE',
+  origin: '*',
+};
+
+app.use(cors(options));
+app.use(express.json());
+routes.map((item) => app.use(item.path, item.router));
+
+const port = process.env.PORT || 3333
+app.listen(port, () => {
+  console.log(`Servidor iniciado na porta: ${port}`)
+})
